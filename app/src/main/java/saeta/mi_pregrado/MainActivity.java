fbe5972316ca88b1c3978c3eaf8f8c46bf252da3
santiagotabchar <santiagocarrenomendoza@gmail.com>
2016-12-26 21:45:24 -0500

    Prueba con spinner funcionando

--- app/src/main/java/saeta/mi_pregrado/MainActivity.java
+++ app/src/main/java/saeta/mi_pregrado/MainActivity.java
@@ -2,12 +2,40 @@ package saeta.mi_pregrado;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import static saeta.mi_pregrado.R.id.planets_spinner;
+
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //metodo spinner
+
+        Spinner planets_spinner = (Spinner) findViewById(R.id.planets_spinner);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.planets_array)){
+            @Override
+            public boolean isEnabled(int position) {
+                return false;
+            }
+        };
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        planets_spinner.setAdapter(adapter);
+
+
     }
+
+
+
+
 }
